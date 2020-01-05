479d86715e257659c0b5918b8d9e1ea04ae53717
Xeladaren <xeladaren@pm.me>
2020-01-05 11:16:22 +0100

    test serial com

--- src/main.c
+++ src/main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 
 int main(int argc, char const *argv[])
 {
 	
-	// test
+	int ttyS0 = open("/dev/ttyS0", O_RDWR) ;
+
+	if (ttyS0 > 0){
+		
+
+
+
+		close(ttyS0) ;
+	}
+	else{
+		printf("ERROR : open /dev/ttyS0 %d\n", ttyS0);
+	}
+
 	printf("Saluton Mondo !\n");
 
 	return 0;
