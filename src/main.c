384105d23ba93109338261061b6938f28ce5cea1
Xeladaren <xeladaren@pm.me>
2020-01-11 19:13:52 +0100

    2020-01-11 19:13

--- src/main.c
+++ src/main.c
@@ -86,7 +86,6 @@ int main(int argc, char const *argv[])
 			memset(command, 0, 100) ;
 			memset(writeMSG, 0, 102) ;
 
-			printf("> ");
 			scanf("%s", command) ;
 
 			if(!strcmp("quit", command)){
